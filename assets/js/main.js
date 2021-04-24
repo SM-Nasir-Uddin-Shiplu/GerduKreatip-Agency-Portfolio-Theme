c7310b916149b01912434bd5a229ca825fb43c83
Nasir Uddin Shiplu <nasiruddinshiplu@gmail.com>
2021-04-24 19:46:31 +0600

    Created a backup branch branchA and modify index.html, main.js and responsive.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,69 +6,26 @@ $(document).ready(function(){
 	
 	viewportHeight(vh);
 	
-
 	/*STICKY MENU AND SECTION SCROLL(WAYPOINTS)*/
 	$('.waypoints').waypoint(function(direction){
 		if ( direction == "down" ) {
-			$('nav').addClass('sticky');
-			//$('.main-menu').css("background-color","transparent");
+			$('nav').addClass('sticky');			
 		} else {
-			$('nav').removeClass('sticky');
-			//$('.main-menu').css("background-color","#1abc9c");
+			$('nav').removeClass('sticky');		
 		}
 	});
 
-	/*MY SOLUTION OF PAGE SCROLL AND STICKY MENU*/
-	/*var distance = $('.menu-section').offset().top;
-
-    $window = $(window);
-
-	$window.scroll(function() {
-	    if ( $window.scrollTop() > distance ) {
-	        // Your div has reached the top
-	        $("nav").addClass("sticky");
-
-	    } else {
-	    	$('nav').removeClass('sticky');
-	    }
-	});*/
-
-
-	/*MENU ACTIVE CLASS*/
-	/*var main = $('#main-menu ul');
-
-	$('.main-nav li a').click(function(event) {
-	        
-	    event.preventDefault();
-	 
-	    var full_url = this.href,
-	        parts = full_url.split('#'),
-	        trgt = parts[1],
-	        target_offset = $('#'+trgt).offset(),
-	        target_top = target_offset.top;
-	        
-	    $('html, body').animate({scrollTop:target_top}, 500);
-	    
-	   // Remove active class on any li when an anchor is clicked 
-	    
-	    main.children().removeClass();
-	    
-	    // Add active class to clicked anchor's parent li 
-	        
-	    $(this).parent().addClass('active');
-
-	});*/
-
 	/*MENU AND SECTION ACTIVE CLASS*/
-	//var lastScrollTop = 0; 
 	$(window).on('scroll',function(){
+		
 		$('.down-arrow').css({
 			"opacity": "0",
 			"transition" : "1s"
 		});
+		
 		clearTimeout($.data(this, 'scrollTimer'));
 	    $.data(this, 'scrollTimer', setTimeout(function() {
-	        // do something
+	        // MOUSE STOPED
 	        $('.down-arrow').css({
 				"opacity": "1",
 				"transition" : "1s"
@@ -76,29 +33,8 @@ $(document).ready(function(){
 	        console.log("Haven't scrolled in 250ms!");
 	    }, 250));
 
-		/*var st = $(this).scrollTop();
-		if (st > lastScrollTop){
-			// downscroll code
-			$('.down-arrow').css({
-				"opacity": "0",
-				"transition" : "1s"
-			});
-		} else {
-			// upscroll code
-			$('.down-arrow').css({
-				"opacity": "0",
-				"transition" : "1s"
-			});
-		}
-		lastScrollTop = st;
-		*/
-
-
-
 		$('section').each(function(){
 		  
-			$(this).removeClass('active');
-
 			if( $('#home').inView() ) {
 		        currentSecName = "home";
 		        $('.main-nav li').each(function(){
